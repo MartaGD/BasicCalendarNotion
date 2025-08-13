87e753fc913ffa891e584d72e3f41128d05e78a9
Marta GD <marta.483@hotmail.com>
2025-08-14 01:05:35 +0200

    js

--- script.js
+++ script.js
@@ -49,64 +49,6 @@ function setCalendar(){
     document.getElementById("dayArea").innerHTML = aux.toLocaleString('default', { weekday: 'long' }).toUpperCase();
 }
 
-function setGreeting(name,hours){
-    if(name==""){
-        name = "Cutie";
-    }
-    let greeting = "Have a nice day, ";
-    if(hours>=5&&hours<=11){ //morning
-        greeting = "Good Morning, ";
-    }else if(hours>=12&&hours<=14){ //afternoon
-        greeting = "Good Afternoon, ";
-    }
-    else if(hours>=15&&hours<=18){ //evening
-        greeting = "Good Evening, ";
-    }else { //night
-        greeting = "Good Night, ";
-    }
-    document.getElementById("greeting").innerHTML = greeting + name+"!";
-    urlWidget.searchParams.set("name",name);
-    document.getElementById("urlArea").innerHTML = urlWidget;
-}
-
-function setDate(date,format){
-    const now = date;
-    let day = now.getDate()<=9 ? "0"+now.getDate() : now.getDate();
-    let month = (now.getMonth()+1)<=9 ? "0"+(now.getMonth()+1) : (now.getMonth()+1);
-    let year = now.getFullYear();
-    let literalDate = "";
-    switch(format){
-         case "dd-mm-yyyy":
-            literalDate = day+"-"+month+"-"+year;
-            break;
-        case "mm/dd/yyyy":
-            literalDate = month+"/"+day+"/"+year;
-            break;
-        case "mm-dd-yyyy":
-            literalDate = month+"-"+day+"-"+year;
-            break;  
-        default: //dd/mm/yyyy
-            literalDate = day+"/"+month+"/"+year;
-            break;
-    }
-    document.getElementById("calendar").innerHTML = literalDate;
-    urlWidget.searchParams.set("format",format);
-    document.getElementById("urlArea").innerHTML = urlWidget;
-}
-
-function getlabel(name){
-    const now = new Date();
-    setGreeting(name,now.getHours());
-    urlWidget.searchParams.set("name",name);
-    document.getElementById("urlArea").innerHTML = urlWidget;
-
-}
-
-function getColor(val){
-    setColor(val);
-
-}
-
 function toggleMode(){
     var element = document.getElementById("preview");
     element.classList.toggle("dark-mode");
